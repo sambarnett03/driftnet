260510138a0170edf2ad71c5208b826d464841be
Sam Barnett <sambarnett2003@gmail.com>
2026-06-11 10:31:48 +0100

    ruff format fixes

--- src/driftnet/data.py
+++ src/driftnet/data.py
@@ -1,5 +1,18 @@
 from pathlib import Path
 import xarray as xr
+import numpy as np
 
-def load_file(fname: Path) -> xr.Dataset:
-    return xr.open_dataset(fname)
+
+def _load_nc_file():
+    pass
+
+
+def nc_file_to_npys():
+    # Load .nc file
+
+    # Loop through each time counter (1440 total) as too big to load all at once
+
+    # Stack u_surf and v_surf
+
+    # Save as .npy file to data folder (read from config) - use datetime as filename
+    pass

c68d4eee6fb04199ab640517cc48ff50da975c45
Sam Barnett <sambarnett2003@gmail.com>
2026-06-11 11:19:06 +0100

    added nc processing to npy functions

--- scripts/experiment_script.py
+++ scripts/experiment_script.py
@@ -1,20 +1,27 @@
 import argparse
+import yaml
+from pathlib import Path
 
+from driftnet.data import nc_file_to_npys
 
 def main():
     parser = argparse.ArgumentParser(description="Run Driftnet")
     parser.add_argument(
-        "--config", type=str, default="configs/default.yaml", help="Path to the config file"
+        "--config", type=str, default="/home/users/sbarnett/documents/driftnet/configs/default.yml", help="Path to the config file"
     )
-    # args = parser.parse_args()
+    args = parser.parse_args()
 
     # Load the configuration file
-    # with open(args.config) as f:
-    #     config = yaml.safe_load(f)
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
 
-    # Pass the config dictionary to your core logic
-    # run_model(config)
+    # Extract data dirs
+    nc_dir = Path(config['data']['nc_directory'])
+    original_res_dir = Path(config['data']['original_res_images'])
 
+    # Run script
+    fname = Path('/gws/pw/j07/wio/WIOInterannual/WINDS_SFC_201504.nc')
+    nc_file_to_npys(fname, original_res_dir)
 
 if __name__ == "__main__":
     main()
--- src/driftnet/data.py
+++ src/driftnet/data.py
@@ -1,18 +1,50 @@
 from pathlib import Path
-
 import xarray as xr
+import numpy as np
+from numpy.typing import NDArray
+
+
+def _extract_u_and_v(
+    ds: xr.Dataset,
+) -> tuple[NDArray[np.floating], NDArray[np.floating]]:
+
+    if 'u_surf' not in list(ds.keys()) or 'v_surf' not in list(ds.keys()):
+        raise KeyError('The variables u_surf or v_surf were not found')
+
+    u = ds.u_surf.values
+    v = ds.v_surf.values
+
+    if u.ndim != 2 or v.ndim != 2:
+        raise ValueError('u or v does not have dimensions 2. Ensure time axis is removed')
+
+    return u, v
+
+
+def _create_npy_file_name(time: np.datetime64) -> str:
+    date_time = str(time)
+    return 'velocities' + date_time + '.npy'
 
 
 def load_file(fname: Path) -> xr.Dataset:
+    if not fname.exists():
+        raise FileNotFoundError(f'File {fname} could not be found ')
     return xr.open_dataset(fname)
 
 
-def nc_file_to_npys():
+def nc_file_to_npys(fname: Path, save_dir: Path):
     # Load .nc file
+    ds = load_file(fname)
 
     # Loop through each time counter (1440 total) as too big to load all at once
+    for t in ds.time_counter.values:
+        print(t)
+        ds_filtered = ds.sel({"time_counter": t})
 
-    # Stack u_surf and v_surf
+        # Stack u_surf and v_surf
+        u, v = _extract_u_and_v(ds_filtered)
+        vel_field = np.stack((u, v))
 
-    # Save as .npy file to data folder (read from config) - use datetime as filename
-    pass
+        # Save as .npy file to data folder (read from config) - use datetime as filename
+        save_fname = _create_npy_file_name(t)
+        np.save(save_dir / save_fname, vel_field)
+    return
